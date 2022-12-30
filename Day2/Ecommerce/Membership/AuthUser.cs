f917055fcd8aeb6168bf4ad007e9a0e3fe1cf3cd
yashgujarthi <yashgujarathi25@gmail.com>
2022-12-30 16:03:57 +0530

    adding Dotnet core structure

---
+++ Day2/Ecommerce/Membership/AuthUser.cs
@@ -0,0 +1,13 @@
+﻿namespace Membership;
+public class AuthUser
+{
+    public static bool Validate(string email, string password){
+        if(email=="yashgujarathi25@gmail.com" && password == "yash2512")
+        {
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+}
---
+++ Day2/Ecommerce/Test/Program.cs
@@ -0,0 +1,17 @@
+﻿using Membership;
+
+Console.WriteLine("**********************************************************");
+Console.WriteLine("Mazak me Tanmay Ka flower");
+Console.WriteLine("**********************************************************");
+
+Console.WriteLine("Enter Email :- ");
+string email = Console.ReadLine();
+Console.WriteLine("Enter Password :- ");
+string password = Console.ReadLine();
+
+if(AuthUser.Validate(email,password)){
+    Console.WriteLine("Welcome to Tanmay ka Flower Bazaar");
+}
+else{
+    Console.WriteLine("Invalid User...!!!!");
+}
